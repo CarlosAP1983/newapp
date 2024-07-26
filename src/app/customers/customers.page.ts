835f2c75d10ffd4a86023f7959ab000259791642
CarlosAP1983 <carlos.arayac@icloud.com>
2024-07-26 11:42:54 -0400

    Update

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; // Importamos HttpClient
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customers',
@@ -8,13 +10,33 @@ import { Router } from '@angular/router';
 })
 export class CustomersPage implements OnInit {
 
-  constructor(private router: Router) { }
+  users: any = [];
+
+  constructor(
+    private router: Router,
+    private http: HttpClient // Inyectamos HttpClient
+  ) { }
 
   ngOnInit() {
+    console.log("holanda holanda que talca!?");
+      this.getuser().subscribe(res=>{
+        console.log("Res", res)
+        this.users = res;
+      });
   }
 
   goToHome(){
     this.router.navigate(['/home'])
   }
 
+  //llamada a customer.json
+  getuser(){
+    return this.http
+    .get("assets/files/customer.json")
+    .pipe(
+      map((res:any) =>{
+        return res.data;
+      })
+    )
+  }
 }
